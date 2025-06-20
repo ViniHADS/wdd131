a3dc9057cf373b7c3f580863ec76c9c9eaed4c2f
Vinicius Araujo <90992850+ViniHADS@users.noreply.github.com>
2025-06-20 12:22:26 -0300

    Project documentation completed

---
+++ project/scripts/siteplan.js
@@ -0,0 +1,13 @@
+// get the current year and insert it into the element with id "currentyear"
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// get the last modified date of the document and insert it into the element with id "lastmodified"
+document.getElementById("lastmodified").textContent = document.lastModified;
+
+// Toggle the navigation menu when the hamburger icon is clicked
+const hamburger = document.getElementById("hamburger");
+const nav = document.querySelector("nav");
+
+hamburger.addEventListener("click", () => {
+  nav.classList.toggle("open");
+});
